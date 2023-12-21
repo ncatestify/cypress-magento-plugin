2511f5dbabc888f13d0aba161afc8e4af41baffc
Sandra Perko-Klomfaß <4748089+knospe-@users.noreply.github.com>
2023-12-21 13:37:34 +0100

    Update General spec.

--- cypress/e2e/general.cy.ts
+++ cypress/e2e/general.cy.ts
@@ -1,10 +1,13 @@
 import selectors from '../fixtures/selectors/startpage.json';
+import {StartPage} from '@/pages/StartPage';
 
 describe('General page tests', function () {
+  const startPage: StartPage = new StartPage();
+
   beforeEach(() => {
-    cy.visit('/');
-    cy.wait(Cypress.env('waitForPageLoad'));
-  })
+    startPage.visit();
+    startPage.waitForPageLoad();
+  });
 
   it('Check if the logo has the title attribute set', function () {
     cy.get(selectors.logoSelector).invoke('attr', 'title').should('not.be.empty');
