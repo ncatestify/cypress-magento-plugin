779f8035e6b479d68e426667841d6bbe683219a9
Sandra Perko-Klomfaß <4748089+knospe-@users.noreply.github.com>
2023-12-22 09:38:54 +0100

    Add a test, that checks if the favicon exists.

--- cypress/e2e/general.cy.ts
+++ cypress/e2e/general.cy.ts
@@ -1,7 +1,7 @@
+import { StartPage } from '@/pages/StartPage';
 import selectors from '../fixtures/selectors/startpage.json';
-import {StartPage} from '@/pages/StartPage';
 
-describe('General page tests', function () {
+describe('General page tests', function() {
   const startPage: StartPage = new StartPage();
 
   beforeEach(() => {
@@ -9,7 +9,11 @@ describe('General page tests', function () {
     startPage.waitForPageLoad();
   });
 
-  it('Check if the logo has the title attribute set', function () {
+  it('Check if the logo has the title attribute set', function() {
     cy.get(selectors.logoSelector).invoke('attr', 'title').should('not.be.empty');
-  })
-})
+  });
+
+  it('Check if the favicon exists', function() {
+    cy.get('head link[rel="shortcut icon"]').should('exist');
+  });
+});
